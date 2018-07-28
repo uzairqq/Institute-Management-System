7b10786bf0b37e73509ce1aaf4ed0f84f909c440
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-28 22:05:30 +0500

    Starting Initial commit

---
+++ InstituteManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InstituteManagementSystem.Startup))]
+namespace InstituteManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
