b4fe7979829bec946640e1b5159878d478a013ac
Uzair Iqbal <uzair.qq@outlook.com>
2019-06-27 15:59:17 +0500

    basic structure

---
+++ src/SchoolManagementSystem.Domain/DataAccessRegistry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagementSystem.Domain
+{
+   public class DataAccessRegistry
+    {
+    }
+}
---
+++ src/SchoolManagementSystem.Services/CommonAccessRegistry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagementSystem.Services
+{
+   public class CommonAccessRegistry
+    {
+    }
+}
